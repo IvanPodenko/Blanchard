4f940f3aaf27e98843d5e5979e1bd8323a12c80e
Ivan <i.podenko@gmail.com>
2023-06-28 21:25:06 +0300

    done swiper + checkbox + choices in gallery

--- src/js/components/gallery-swiper.js
+++ src/js/components/gallery-swiper.js
@@ -1,8 +1,18 @@
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper('.gallery__swiper', {
-  modules: [Navigation],
+  modules: [Navigation, Pagination],
   slidesPerView: 3,
   spaceBetween: 30,
+
+  pagination: {
+    el: ".gallery__right-pagination",
+    type: "fraction",
+  },
+
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  }
 });
 
