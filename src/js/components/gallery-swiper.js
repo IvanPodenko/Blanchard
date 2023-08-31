60fad2f8d76a4ebb60b7aea187aee1f16e53e420
Ivan <i.podenko@gmail.com>
2023-08-31 22:56:10 +0300

    gallery new v2.1

--- src/js/components/gallery-swiper.js
+++ src/js/components/gallery-swiper.js
@@ -19,13 +19,13 @@ const swiper = new Swiper('.gallery__swiper', {
 
     441: {
       slidesPerView: 2,
-      spaceBetween: 38
+      spaceBetween: 30
     },
 
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 34
-    },
+    // 1024: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 34
+    // },
 
     1200: {
       slidesPerView: 3,
